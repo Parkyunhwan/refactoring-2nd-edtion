2ee2efc833d2b32636f6b2889ac8cab2e3664fae
parkyunhwan <pyh8618@gmail.com>
2025-01-22 00:45:01 +0900

    refactor: 공연금액계산로직 함수 추출 리팩터링

--- src/main/java/com/refactoring/chapter1/Statement.java
+++ src/main/java/com/refactoring/chapter1/Statement.java
@@ -4,7 +4,6 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Statement {
-    int totalAmount = 0;
     public String statement(Invoice invoice, Plays plays) {
         int totalAmount = 0;
         int volumeCredits = 0;
@@ -16,25 +15,7 @@ public class Statement {
 
         for (Invoice.Performance perf : invoice.getPerformances()) {
             Play play = plays.get(perf.getPlayID());
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new RuntimeException("알 수 없는 장르: " + play.getType());
-            }
+            int thisAmount = amountFor(perf, play);
 
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
             if ("comedy".equals(play.getType())) {
@@ -49,5 +30,28 @@ public class Statement {
         result.append(String.format("적립 포인트: %s점", volumeCredits));
 
         return result.toString();
+    }
+
+    private static int amountFor(Invoice.Performance perf, Play play) {
+        int thisAmount = 0;
+
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.getAudience() > 30) {
+                    thisAmount += 1000 * (perf.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                }
+                thisAmount += 300 * perf.getAudience();
+                break;
+            default:
+                throw new RuntimeException("알 수 없는 장르: " + play.getType());
+        }
+        return thisAmount; // 값이 바뀌는 변수값 반환
     }
 }
